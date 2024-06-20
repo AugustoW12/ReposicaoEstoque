56c7d885d01ec02da4491fe6446f86f405af8284
Augusto <55516045+AugustoW12@users.noreply.github.com>
2024-06-20 16:45:09 -0300

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getCurrentDateTime() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    return `${day}-${month}-${year}`;//-${hours}:${minutes}:${seconds}`;
+    return `${day}-${month}-${year}-${hours}:${minutes}:${seconds}`;
 }
 
 // Define a data e hora atuais no campo oculto
